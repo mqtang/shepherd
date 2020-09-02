c8cfd3bfc88e507ea11a73f13377fc819461c637
itangcheng <itangcheng@163.com>
2020-09-02 22:58:37 +0800

    add spring web

---
+++ shepherd-server/src/main/java/guru/bootstrap/shepherd/ShepherdApplication.java
@@ -0,0 +1,15 @@
+package guru.bootstrap.shepherd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tangcheng
+ */
+@SpringBootApplication
+public class ShepherdApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShepherdApplication.class, args);
+    }
+}
+// 2020/9/2 22:20
